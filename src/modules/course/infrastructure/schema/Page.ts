63edd1eb426ccf4736d8d72648984632df6e75c8
mfaour34 <faour606@gmail.com>
2022-04-22 23:09:38 +0300

    add page schema

--- src/modules/course/infrastructure/schema/Page.ts
+++ src/modules/course/infrastructure/schema/Page.ts
@@ -2,14 +2,14 @@ import { IComponent, IPage } from '@course/application/entities/IPage'
 import { Schema, model } from 'mongoose'
 
 export const componentSchema = new Schema<IComponent>({
-    paragraph:{type:String,required:true},
-    photo:{type:String,required:false},
-    textInput:{type:String,required:true},
-    video:{type:String,required:false}
+  paragraph: { type: String, required: true },
+  photo: { type: String, required: false },
+  textInput: { type: String, required: true },
+  video: { type: String, required: false },
 })
 
 const pageSchema = new Schema<IPage>({
-    components:
+  components: { type: [componentSchema], required: true },
   lessonId: { type: Schema.Types.ObjectId, ref: 'Lesson' },
 })
 
